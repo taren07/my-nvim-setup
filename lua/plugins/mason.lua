2eb3b2575fe07b90249b3387da88a43a5c7a0292
taren07 <taro.enchi@gmail.com>
2024-09-28 22:18:51 +0900

    bug fix

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,31 +1,3 @@
 local mason = require('mason')
-local lspconfig = require('mason-lspconfig')
 
 mason.setup {}
-lspconfig.setup {
-    ensure_installed = {
-        'html',         -- HTML
-        'cssls',        -- CSS
-        'bashls',       -- Bash
-        'dockerls',     -- Dockerfile
-        'vimls',        -- Vim
-        'marksman',     -- Markdown
-        'phpactor',     -- PHP
-        'solargraph',   -- Ruby
-        'cssmodules_ls',-- SCSS
-        'tailwindcss',  -- Tailwind CSS
-        'hls',          -- Haskell
-        'gopls',        -- Golang
-    }
-}
-
-require'lspconfig'.tailwindcss.setup {}
-require'lspconfig'.golangci_lint_ls.setup{}
-require'lspconfig'.dhall_lsp_server.setup{}
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-require'lspconfig'.html.setup {
-  capabilities = capabilities,
-}
